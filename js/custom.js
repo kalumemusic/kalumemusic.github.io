7fee997843ebc9bd76340c9d4b375db070243a86
kalumemusic <47136499+kalumemusic@users.noreply.github.com>
2019-02-01 14:59:18 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         userId: '45852833',
         limit: 12,
         resolution: 'standard_resolution',
-        accessToken: '45852833.1677ed0.99dc02dc246e4d1186adf316f892398b',
+        accessToken: '45852833.1677ed0.211adb6f59764b8eb2e9a622bb781c3c',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
